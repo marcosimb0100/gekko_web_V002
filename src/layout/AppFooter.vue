4910df8263e39145ce5b4422b98848652348366c
marcosimb0100 <marcosimb@gmail.com>
2026-08-07 19:00:06 -0600

    Cambios

--- src/layout/AppFooter.vue
+++ src/layout/AppFooter.vue
@@ -1,8 +1,12 @@
-<script setup></script>
+<script setup>
+    const appCliente = import.meta.env.VITE_APP_CLIENT;
+    const appVersion = import.meta.env.VITE_APP_VERSION;
+</script>
 
 <template>
     <div class="layout-footer">
-        SAKAI by
-        <a href="https://primevue.org" target="_blank" rel="noopener noreferrer" class="text-primary font-bold hover:underline">PrimeVue</a>
+        GEKKO by
+        <!-- <a href="https://primevue.org" target="_blank" rel="noopener noreferrer" class="text-primary font-bold hover:underline">{{appCliente}}</a> -->
+        <label class="text-primary font-bold">{{appCliente}}</label>
     </div>
 </template>
